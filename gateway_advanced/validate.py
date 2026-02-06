5033d359666e885e4213488742355c8206e6a8b0
pdefusco <pauldefusco@gmail.com>
2026-02-06 15:15:23 -0800

    added API key to validate requests

--- gateway_advanced/validate.py
+++ gateway_advanced/validate.py
@@ -1,12 +1,18 @@
 import requests
 import os
 
-port = os.environ.get("CDSW_APP_PORT", "8080")
+GATEWAY_URL = os.environ.get("GATEWAY_URL")
+TOKEN = os.getenv("API_KEY")  # set this in your environment
 
+headers = {"Authorization": f"Bearer {TOKEN}"}
 # Test ping
-r = requests.get(f"http://127.0.0.1:{port}/ping")
+r = requests.get(f"{GATEWAY_URL}/ping", headers=headers)
 print("Ping:", r.json())
 
 # Test inference
-r = requests.post(f"http://127.0.0.1:{port}/inference", json={"inputs": "What is a finite state machine?"})
+
+question = "What is a finite state machine?"
+payload = {"inputs": question}
+
+r = requests.post(f"{GATEWAY_URL}/inference", headers=headers, json=payload)
 print("Inference output:", r.json())
