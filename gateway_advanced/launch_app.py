89f90ccab76f6c42befb9e532b3dda04d03d45a2
pdefusco <pauldefusco@gmail.com>
2026-02-06 18:38:45 -0800

    port

--- gateway_advanced/launch_app.py
+++ gateway_advanced/launch_app.py
@@ -1,1 +1,1 @@
-!streamlit run /home/cdsw/gateway_advanced/dashboard.py --server.port 8080 --server.address 127.0.0.1
+!streamlit run /home/cdsw/gateway_advanced/dashboard.py --server.port $CDSW_READONLY_PORT --server.address 127.0.0.1
